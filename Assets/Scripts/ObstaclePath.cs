7901a1fd4a7d6d2bcd833782d13b7a1590844644
Leon Freire <leonvfreire@gmail.com>
2020-01-22 12:36:22 -0800

    - Added movement by path

---
+++ Assets/Scripts/ObstaclePath.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePath : MonoBehaviour
+{
+    [SerializeField]
+    Transform pathPointHolder;
+    [SerializeField]
+    float speed;
+    List<Vector3> pathPoints = new List<Vector3>();
+    int targetPathPointIndex = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathPoints.Add(transform.position);
+        foreach (Transform point in pathPointHolder)
+            pathPoints.Add(point.position);
+
+        StartCoroutine(MoveAlongPath());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator MoveAlongPath()
+    {
+        while (true)
+        {
+            Vector3 targetPosition = pathPoints[targetPathPointIndex];
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.fixedDeltaTime * speed);
+
+            if (transform.position == targetPosition)
+            {
+                targetPathPointIndex = GetNextPathPointIndex();
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    int GetNextPathPointIndex()
+    {
+        int newIndex = targetPathPointIndex;
+        if (newIndex < pathPoints.Count - 1)
+        {
+            newIndex++;
+        }
+        else
+        {
+            newIndex = 0;
+        }
+        return newIndex;
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 1; i < pathPoints.Count; ++i)
+        {
+            Gizmos.DrawLine(pathPoints[i], pathPoints[i - 1]);
+        }
+    }
+}

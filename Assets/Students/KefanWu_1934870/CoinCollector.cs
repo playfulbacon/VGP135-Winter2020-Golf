874f9fb2bf2bc1a80df3660a48e7aba29a5a1311
AndyBacon <bacon.andrew@gmail.com>
2020-01-29 11:08:51 -0800

    Coin Collector.

---
+++ Assets/Students/KefanWu_1934870/CoinCollector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CoinController coinController = other.GetComponent<CoinController>();
+        if (coinController)
+        {
+            coinController.Collect();
+        }
+    }
+}

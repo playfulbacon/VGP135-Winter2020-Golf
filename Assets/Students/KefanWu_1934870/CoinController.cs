ef0e1e57df716ed0949855569441b1deb233c74c
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2020-01-29 09:46:20 -0800

    - update assignment1

---
+++ Assets/Students/KefanWu_1934870/CoinController.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+
+    public float SpinSpeed;
+    private float startHeight = 0.0f;
+    private float bounce = 0.0f;
+    private bool collected = false;
+
+
+    // Update is called once per frame
+    void Update()
+ 
+    {
+        transform.Rotate(Vector3.right, SpinSpeed * Time.deltaTime);
+        if (collected)
+        {
+            bounce += Time.deltaTime * 10.0f;
+            Vector3 postion = transform.position;
+            postion.y = startHeight + Mathf.Sin(bounce) * 2.0f;
+            transform.position = postion;
+            if (bounce >= Mathf.PI)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+    public void Collect()
+    {
+        startHeight = transform.position.y;
+        SpinSpeed *= 10.0f;
+        collected = true;
+    }
+}

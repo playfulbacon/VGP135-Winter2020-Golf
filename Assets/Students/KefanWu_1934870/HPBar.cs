c449928b7a6b8f50424916884c68698d9de35416
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2020-01-29 09:29:54 -0800

    -Assignment1

---
+++ Assets/Students/KefanWu_1934870/HPBar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HPBar : MonoBehaviour
+{
+    public Slider slider;
+    public float health;
+    public float resultHealth;
+    private bool obstacles = false;
+
+
+
+
+
+    void Start()
+    {
+        slider.maxValue = 100;
+        slider.minValue = 0;
+        slider.value = slider.maxValue;
+        health = slider.value;
+        resultHealth = health;
+
+    }
+
+
+    void Update()
+    {
+        if(obstacles)
+        {
+            resultHealth = slider.value - 10 < slider.value ? slider.minValue : slider.value - 10;
+            obstacles = false;
+        }
+        health = Mathf.Lerp(health, resultHealth, 0.05f);
+        slider.value = health;
+        if(slider.value<=0.01)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+
+    }
+    public void ReduceHealth()
+    {
+        obstacles = true;
+    }
+
+}
---
+++ Assets/Students/KefanWu_1934870/Switch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Switch : MonoBehaviour
+
+{
+    public Switch toggleswitch;
+    GameObject obstacles;
+
+    public void OnCollisionEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            obstacles = GameObject.FindGameObjectWithTag("obstacles");
+            obstacles.SetActive(false);
+        }
+    }
+}

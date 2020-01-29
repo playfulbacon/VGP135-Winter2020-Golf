6896ccd338ad034d7f5de0066d8c3b4b340fcd5b
AndyBacon <bacon.andrew@gmail.com>
2020-01-29 12:03:18 -0800

    Pickup.

---
+++ Assets/Scripts/PickupExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupExample : Pickup
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void OnPickup()
+    {
+        base.OnPickup();
+        print("pickup example");
+    }
+}

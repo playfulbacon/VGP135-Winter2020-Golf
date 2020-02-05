405612d3dd4fe9e2609dbbfa756917fef896ce7f
Henry Liao <liaohenry2310@gmail.com>
2020-02-05 12:17:34 -0800

    Created HenryLiao's Level - Added HenryLevel scene. - Added LaunchPad script to set triggers to launch objects into the air. - Added SpinsOnY script to make objects spin on world Y axis. - Added BackAndForthMove script to move objects back and forth between two positions.

---
+++ Assets/Students/HenryLiao/SpinsOnY.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinsOnY : MonoBehaviour
+{
+    public float spinSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0.0f, spinSpeed, 0.0f) * Time.deltaTime, Space.World);
+    }
+}

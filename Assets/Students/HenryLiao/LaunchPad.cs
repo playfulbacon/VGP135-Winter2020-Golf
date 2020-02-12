c35c3c569be316a58127afa1415709aa0ff5f280
liaohenry2310 <liaohenry2310@gmail.com>
2020-02-12 10:34:54 -0800

    Updated LaunchPad script

--- Assets/Students/HenryLiao/LaunchPad.cs
+++ Assets/Students/HenryLiao/LaunchPad.cs
@@ -7,8 +7,8 @@ public class LaunchPad : MonoBehaviour
     public float launchSpeed = 10.0f;
     private void OnTriggerEnter(Collider other)
     {
-        Transform ball = other.transform.parent;
-        if (ball != null && ball.name == "Ball")
+        Ball ball = other.GetComponentInParent<Ball>();
+        if (ball)
         {
             Rigidbody rb = ball.GetComponent<Rigidbody>();
             rb.velocity += new Vector3(0.0f, launchSpeed, 0.0f);

a91ead3e982a1c943b6870e1f2d7f223ca75de77
Stan Gerard Palisoc <stanpal@hotmail.com>
2020-02-19 12:48:07 -0800

    -Created a randomly procedural map that instansiates the game objects in a random position

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public GameObject goalPrefab;
+    public GameObject planePrefab;
+    public GameObject obstaclePrefab;
+
+    private int gridSize = 10;
+    List<Vector2> reservedPositions = new List<Vector2>();
+    List<Vector2> freeGridSpaces = new List<Vector2>();
+
+    private void Awake()
+    {
+        GenerateLevel();
+    }
+
+    private void GenerateLevel()
+    {
+        for(int y = 0; y < gridSize; ++y)
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                freeGridSpaces.Add(new Vector2(x, y));
+            }
+        }
+
+        GameObject ball = Instantiate(ballPrefab);
+        RandomlyPositionObjectOnGrid(ball);
+        ball.transform.position += Vector3.up * 2f;
+
+        //Create plane based on grid size
+        GameObject plane = Instantiate(planePrefab);
+        plane.transform.localScale = new Vector3(gridSize / 5f, 1, gridSize / 5f);
+
+        GameObject goal = Instantiate(goalPrefab);
+        RandomlyPositionObjectOnGrid(goal);
+
+
+        //Create goal and randomize position
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject obstacle = Instantiate(obstaclePrefab);
+            RandomlyPositionObjectOnGrid(obstacle);           
+        }
+    }
+
+    void RandomlyPositionObjectOnGrid(GameObject gameObject)
+    {
+        if (freeGridSpaces.Count == 0)
+        {
+            Debug.Log("no free spaces left in the grid");
+            return;
+        }
+        Vector2 gridSpace = freeGridSpaces[Random.Range(0, freeGridSpaces.Count)];
+        freeGridSpaces.Remove(gridSpace);
+        Vector3 gridPosition = new Vector3(-gridSize / 2 + gridSpace[0] - 0, 0, -gridSize / 2 + gridSpace[1]);
+        gameObject.transform.position = gridPosition;
+    }
+}

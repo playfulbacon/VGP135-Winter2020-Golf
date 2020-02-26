b1061a9116f9cf1223aacf3b2fcdab6d9e10abcc
christy <keisuke.sato@lasallecollegevancouver.com>
2020-02-26 12:27:37 -0800

    -added enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    enum State { Idle,Pursue, Attack};
+    State state = State.Idle;
+    Ball[] balls;
+    Ball pursuingBall;
+    Rigidbody rb;
+    enemyanimatorhelper enemyanimatorhelper;
+    private void Awake()
+    {
+        enemyanimatorhelper = GetComponentInChildren<enemyanimatorhelper>();
+        enemyanimatorhelper.OnAttack += OnAttacke;
+        enemyanimatorhelper.onAttackComplate += OnAttackComplate;
+    }
+    void OnAttacke()
+    {
+
+    }
+    void OnAttackComplate()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        balls = FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

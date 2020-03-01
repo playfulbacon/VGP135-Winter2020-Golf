cbde3ea061a0f09204dbbeff21ca75eb2332f45c
YuHan Ma <59942793+orangutanz@users.noreply.github.com>
2020-02-29 20:18:43 -0800

    20190229
    20190229

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public float minGoalRange = 5f;    
+    public float maxGoalRange = 15f;
+    public GameObject ballPrefab;
+    public GameObject goalPrefab;
+    public GameObject planePrefab;
+
+    private void Awake()
+    {
+        GenerateLevel();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateLevel()
+    {
+        Instantiate(ballPrefab);
+
+        //GameObject goal = Instantiate(goalPrefab);
+  
+        Instantiate(goalPrefab);
+        Instantiate(planePrefab);
+
+    }
+}

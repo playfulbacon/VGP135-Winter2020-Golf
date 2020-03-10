9c0e3489b63789bf16a41fffe691f52c0e10cf37
Jaime Zedan <zedanjaime@gmail.com>
2020-03-10 11:02:57 -0700

    -added skill jump

---
+++ Assets/Scripts/Skill_Jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_Jump : MonoBehaviour
+{
+    public float jumpVelocity;
+
+    float maxDistance = 0.5f;
+    bool isJumping = false;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, maxDistance))
+        {
+            isJumping = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector3(0, jumpVelocity, 0), ForceMode.Impulse);
+            isJumping = true; 
+        }
+    }
+
+
+}

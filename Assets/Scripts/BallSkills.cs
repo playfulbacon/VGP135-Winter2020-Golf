af30acd92cb503b4aa2282af6ad4cd2d19d634ea
Leon Freire <leonvfreire@gmail.com>
2020-03-11 08:57:43 -0700

    Included Fire Skill and Wall breaking

---
+++ Assets/Scripts/BallSkills.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSkills : MonoBehaviour
+{
+    Ball ball;
+    ParticleSystem ps;
+    public bool fireSkill = false;
+
+    private void Awake()
+    {
+        ball = GetComponent<Ball>();
+        ps = ball.GetComponentInChildren<ParticleSystem>();
+    }
+
+    void Start()
+    {
+        if (ps != null)
+        {
+            Instantiate(ps);
+            ps.Stop();
+        }
+    }
+    
+    void Update()
+    {
+        if (!fireSkill)
+        {
+            if (Input.GetKeyDown("1"))
+            {
+                ps.Play();
+                fireSkill = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown("1"))
+            {
+                ps.Stop();
+                fireSkill = false;
+            }
+        }
+        ps.transform.rotation = Quaternion.Euler(-90.0f, 0.0f, gameObject.transform.rotation.z * -1.0f);
+    }
+}

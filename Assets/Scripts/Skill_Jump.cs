c4a69af8bc5979f585367366c128301114bc8549
Henry Liao <liaohenry2310@gmail.com>
2020-03-11 10:03:03 -0700

    Added Level modifier in Skill-Jump. - Changed jump power to be velocity-based from force-based. - Fixed bug where ball could unexpectantly air-jump.

--- Assets/Scripts/Skill_Jump.cs
+++ Assets/Scripts/Skill_Jump.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Skill_Jump : MonoBehaviour
 {
-    public float jumpVelocity;
-
-    float maxDistance = 0.5f;
+    int currentLevel = 0;
+    float jumpVelocity = 0.0f;
+    float maxDistance = 0.251f;
     bool isJumping = false;
+    bool toJump = false;
 
     Rigidbody rb;
 
@@ -16,8 +17,17 @@ public class Skill_Jump : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SetSkillLevel(currentLevel + 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            SetSkillLevel(currentLevel - 1);
+        }
+
         Ray ray = new Ray(transform.position, Vector3.down);
 
         if (Physics.Raycast(ray, maxDistance))
@@ -25,12 +35,32 @@ public class Skill_Jump : MonoBehaviour
             isJumping = false;
         }
 
-        if (Input.GetButtonDown("Jump") && !isJumping)
+        if (currentLevel > 0 && toJump && !isJumping)
         {
-            rb.AddForce(new Vector3(0, jumpVelocity, 0), ForceMode.Impulse);
-            isJumping = true; 
+            rb.velocity = new Vector3(rb.velocity.x, jumpVelocity, rb.velocity.z);
+            isJumping = true;
+            toJump = false;
         }
     }
 
+    void Update()
+    {
+        if (currentLevel > 0 && !isJumping && Input.GetButtonDown("Jump"))
+        {
+            toJump = true;
+        }
+    }
 
+    public void SetSkillLevel(int level)
+    {
+        currentLevel = Mathf.Clamp(level, 0, 3);
+        if (currentLevel > 0)
+        {
+            jumpVelocity = 2.0f + (currentLevel * 3.0f);
+        }
+        else
+        {
+            jumpVelocity = 0.0f;
+        }
+    }
 }
